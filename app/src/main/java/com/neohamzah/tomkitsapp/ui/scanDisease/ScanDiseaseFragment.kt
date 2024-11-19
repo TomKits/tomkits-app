4c90b3102751226dffa0b25a62f62027cb066b19
NeoHamzah <amirulhamzah758@gmail.com>
2024-11-19 16:55:43 +0700

    feat(all): restructuring the folder, add icon for bottom nav, and change bottom nav

--- app/src/main/java/com/neohamzah/tomkitsapp/ui/scanDisease/ScanDiseaseFragment.kt
+++ app/src/main/java/com/neohamzah/tomkitsapp/ui/scanDisease/ScanDiseaseFragment.kt
@@ -1,4 +1,4 @@
-package com.neohamzah.tomkitsapp.ui.dashboard
+package com.neohamzah.tomkitsapp.ui.scanDisease
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.neohamzah.tomkitsapp.databinding.FragmentDashboardBinding
+import com.neohamzah.tomkitsapp.databinding.FragmentScanDiseaseBinding
 
-class DashboardFragment : Fragment() {
+class ScanDiseaseFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentScanDiseaseBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val homeViewModel =
+            ViewModelProvider(this).get(ScanDiseaseViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentScanDiseaseBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/neohamzah/tomkitsapp/ui/scanQuality/ScanQualityViewModel.kt
+++ app/src/main/java/com/neohamzah/tomkitsapp/ui/scanQuality/ScanQualityViewModel.kt
@@ -1,13 +1,13 @@
-package com.neohamzah.tomkitsapp.ui.notifications
+package com.neohamzah.tomkitsapp.ui.scanQuality
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ScanQualityViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
